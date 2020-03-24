4092428d275fc97ac33a5e09728e7c2fee2135ce
Elias <eGasparArellano@gmail.com>
2020-03-23 21:29:21 -0600

    Filters now work

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,13 +10,16 @@ export class ProductsComponent implements OnInit {
   listOfProducts: Product[] = [];
   filteredProducts: Product[] = [];
   searchString = '';
+  availabilityFilter = false;
+  sortByPriceFilter = false;
+  quantityFilter = false;
 
   constructor() {
     this.listOfProducts.push(
-      new Product(1, 'Televisión', 'LG', '59 pulgadas, plana', 6000, 2),
+      new Product(1, 'Televisión', 'LG', '59 pulgadas, plana', 6000, 0),
       new Product(2, 'Computadora', 'Acer', '250 SSD, 8GB RAM', 12000, 7),
       new Product(3, 'Monitor', 'BENQ', '30 pulgadas curvo', 35000, 4),
-      new Product(4, 'Audífonos', 'Sony', 'sencillos, cable 1m', 120, 10),
+      new Product(4, 'Audífonos', 'Sony', 'sencillos, cable 1m', 120, 2),
       new Product(5, 'Mouse', 'Logitech', 'Inalámbrico color rojo', 200, 9)
     );
     this.filteredProducts = this.listOfProducts.slice(0);
@@ -26,7 +29,28 @@ export class ProductsComponent implements OnInit {
   }
 
   searchProducts(): void {
-    this.filteredProducts = this.listOfProducts.filter((product) => {
+    this.filteredProducts = this.listOfProducts.slice(0);
+
+    if (this.availabilityFilter) {
+      this.filteredProducts = this.filteredProducts.filter((product) => {
+        return product.quantity > 0;
+      });
+    }
+
+    if (this.sortByPriceFilter) {
+      this.filteredProducts = this.filteredProducts.sort((a, b) => {
+        if (a.price > b.price) {
+          return 1;
+        }
+        if (a.price < b.price) {
+          return -1;
+        }
+        return 0;
+      });
+    }
+    
+    
+    this.filteredProducts = this.filteredProducts.filter((product) => {
       return  product.name.toUpperCase().includes(this.searchString.toUpperCase()) ||
               product.description.toUpperCase().includes(this.searchString.toUpperCase());
     });
